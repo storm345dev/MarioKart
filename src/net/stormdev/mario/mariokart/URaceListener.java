84997004a7f0aef61875563f1703009ac1ae4819
storm345 <storm345dev@gmail.com>
2013-12-02 17:44:19 +0000

    Fixed another issue caused by 'that PR'

--- src/net/stormdev/mario/mariokart/URaceListener.java
+++ src/net/stormdev/mario/mariokart/URaceListener.java
@@ -331,205 +331,213 @@ public class URaceListener implements Listener {
 	// Much extranious PORTED code from here on (Races)
 	@EventHandler(priority = EventPriority.HIGHEST)
 	void RaceEnd(RaceFinishEvent event) {
-		Race game = event.getRace();
-		Boolean timed = game.getType() == RaceType.TIME_TRIAL;
-		List<User> usersIn = game.getUsersIn();
-		String in = "";
-		for (User user : usersIn) {
-			in = in + ", " + user.getPlayerName();
-		}
-		Map<String, Double> scores = new HashMap<String, Double>();
-		Boolean finished = false;
-		User user = event.getUser();
-		Player player = null;
 		try {
-			player = user.getPlayer(plugin.getServer());
-		} catch (PlayerQuitException e1) {
-			//Player has left
-		}
-		if(player != null){
-			player.removeMetadata("car.stayIn", plugin);
-			player.setCustomName(ChatColor.stripColor(player.getCustomName()));
-			player.setCustomNameVisible(false);
-			if (player.getVehicle() != null) {
-				Vehicle veh = (Vehicle) player.getVehicle();
-
-				veh.eject();
-
-				veh.remove();
+			Race game = event.getRace();
+			Boolean timed = game.getType() == RaceType.TIME_TRIAL;
+			List<User> usersIn = game.getUsersIn();
+			String in = "";
+			for (User user : usersIn) {
+				in = in + ", " + user.getPlayerName();
 			}
-			Location loc = game.getTrack().getExit(plugin.getServer());
-			if (loc == null) {
-				player.teleport(player.getLocation().getWorld().getSpawnLocation());
-			} else {
-				player.teleport(loc);
+			Map<String, Double> scores = new HashMap<String, Double>();
+			Boolean finished = false;
+			User user = event.getUser();
+			Player player = null;
+			try {
+				player = user.getPlayer(plugin.getServer());
+			} catch (PlayerQuitException e1) {
+				//Player has left
 			}
-			if (player.isOnline()) {
-				player.getInventory().clear();
+			if(player != null){
+				player.removeMetadata("car.stayIn", plugin);
+				player.setCustomName(ChatColor.stripColor(player.getCustomName()));
+				player.setCustomNameVisible(false);
+				if (player.getVehicle() != null) {
+					Vehicle veh = (Vehicle) player.getVehicle();
 
-				player.getInventory().setContents(user.getOldInventory());
-			}
-		}
-		if (user.isFinished()) {
-			finished = true;
-		} else {
-			HashMap<User, Double> checkpointDists = new HashMap<User, Double>();
-			for (User u : game.getUsers()) {
-				try {
-					Player pp = u.getPlayer(plugin.getServer());
-					if (pp != null){
-						if (pp.hasMetadata("checkpoint.distance")) {
-							List<MetadataValue> metas = pp
-									.getMetadata("checkpoint.distance");
-							checkpointDists.put(u,  (Double) ((StatValue) metas.get(0)).getValue());
-						}
-					}
-				} catch (PlayerQuitException e) {
-					//Player has left
+					veh.eject();
+
+					veh.remove();
 				}
-			}
-
-			for (User u : game.getUsers()) {
-				int laps = game.totalLaps - u.getLapsLeft() + 1;
-
-				int checkpoints = u.getCheckpoint();
-
-				double distance = 1 / (checkpointDists.get(u));
-
-				double score = (laps * game.getMaxCheckpoints()) + checkpoints + distance;
-
-				try {
-					if (game.getWinner().equals(u)) {
-						score = score + 1;
-					}
-				} catch (Exception e) {
-				}
-				scores.put(u.getPlayerName(), score);
-			}
-		}
-		player.getInventory().clear();
-
-		player.getInventory().setContents(user.getOldInventory());
-
-		if (!finished) {
-			DoubleValueComparator com = new DoubleValueComparator(scores);
-			SortedMap<String, Double> sorted = new TreeMap<String, Double>(com);
-			sorted.putAll(scores);
-			Set<String> keys = sorted.keySet();
-			Object[] pls = (Object[]) keys.toArray();
-			for (int i = 0; i < pls.length; i++) {
-				Player p = plugin.getServer().getPlayer((String) pls[i]); //Evidence the dodgy PR was not tested as it was still reading string with Player in the map
-				if (p.equals(player)) {
-					if (p != null) {
-						String msg = "";
-						if (!timed) {
-							msg = main.msgs.get("race.end.position");
-							if ((i + 1) <= 4
-									&& (i + 1) != game.getUsers().size()) {player.getWorld().playSound(player.getLocation(), Sound.NOTE_BASS_GUITAR, 1, 1);
-							} else {
-								player.getWorld().playSound(
-										player.getLocation(), Sound.NOTE_BASS,
-										1, 1);
-							}
-							String pos = "" + (i + 1);
-							if (pos.endsWith("1")) {
-								pos = pos + "st";
-							} else if (pos.endsWith("2")) {
-								pos = pos + "nd";
-							} else if (pos.endsWith("3")) {
-								pos = pos + "rd";
-							} else {
-								pos = pos + "th";
-							}
-							msg = msg.replaceAll("%position%", "" + pos);
-						} else {
-							double tim = (game.endTimeMS - game.startTimeMS) / 10;
-							double ti = (int) tim;
-							double t = ti / 100;
-							msg = main.msgs.get("race.end.time");
-							msg = msg.replaceAll(Pattern.quote("%time%"), t
-									+ "");
-							plugin.raceTimes.addRaceTime(game.getTrack()
-									.getTrackName(), player.getName(), t);
-						}
-						p.sendMessage(main.colors.getSuccess() + msg);
-					}
-				}
-			}
-		} else {
-			if (player != null) {
-				int position = 1;
-
-				for (int i = 0; i < game.getUsersFinished().size(); i++) {
-					if (game.getUsersFinished().get(i).equals(user)) {
-						position = i + 1;
-					}
-				}
-				String msg = "";
-				if (!timed) {
-					msg = main.msgs.get("race.end.position");
-					if (position <= 4 && position != game.getUsers().size()) {
-						player.getWorld().playSound(player.getLocation(),
-								Sound.NOTE_BASS_GUITAR, 1, 1);
-					} else {
-						player.getWorld().playSound(player.getLocation(),
-								Sound.NOTE_BASS, 1, 1);
-					}
-					String pos = "" + position;
-					if (pos.endsWith("1")) {
-						pos = pos + "st";
-					} else if (pos.endsWith("2")) {
-						pos = pos + "nd";
-					} else if (pos.endsWith("3")) {
-						pos = pos + "rd";
-					} else {
-						pos = pos + "th";
-					}
-					try {
-						msg = msg.replaceAll("%position%", "" + pos);
-					} catch (Exception e) {
-					}
+				Location loc = game.getTrack().getExit(plugin.getServer());
+				if (loc == null) {
+					player.teleport(player.getLocation().getWorld().getSpawnLocation());
 				} else {
-					// Time trial
-					double tim = (game.endTimeMS - game.startTimeMS) / 10;
-					double ti = (int) tim;
-					double t = ti / 100;
-					msg = main.msgs.get("race.end.time");
-					msg = msg.replaceAll(Pattern.quote("%time%"), t + "");
-					plugin.raceTimes
-					.addRaceTime(game.getTrack().getTrackName(),
-							player.getName(), t);
+					player.teleport(loc);
 				}
-				player.sendMessage(main.colors.getSuccess() + msg);
-			}
-		}
-		game.leave(user, false);
-		plugin.gameScheduler.updateGame(game);
-		if (game.getUsersIn().size() < 1) {
-			game.ended = true;
-			game.end();
-		}
-		final Player pl = player;
-		plugin.getServer().getScheduler().runTaskLater(plugin, new Runnable() {
+				if (player.isOnline()) {
+					player.getInventory().clear();
 
-			public void run() {
-				String rl = main.config
-						.getString("mariokart.resourceNonMarioPack");
-				Boolean valid = true;
-				try {
-					new URL(rl);
-				} catch (MalformedURLException e2) {
-					valid = false;
+					player.getInventory().setContents(user.getOldInventory());
 				}
-				if (valid) {
-					pl.sendMessage(main.colors.getInfo()
-							+ main.msgs.get("resource.clear"));
-					pl.setTexturePack(rl);
-				}
-				return;
 			}
-		}, 150l);
-		return;
+			if (user.isFinished()) {
+				finished = true;
+			} else {
+				HashMap<User, Double> checkpointDists = new HashMap<User, Double>();
+				for (User u : game.getUsers()) {
+					try {
+						Player pp = u.getPlayer(plugin.getServer());
+						if (pp != null){
+							if (pp.hasMetadata("checkpoint.distance")) {
+								List<MetadataValue> metas = pp
+										.getMetadata("checkpoint.distance");
+								checkpointDists.put(u,  (Double) ((StatValue) metas.get(0)).getValue());
+							}
+						}
+					} catch (PlayerQuitException e) {
+						//Player has left
+					}
+				}
+
+				for (User u : game.getUsers()) {
+					try {
+						int laps = game.totalLaps - u.getLapsLeft() + 1;
+
+						int checkpoints = u.getCheckpoint();
+
+						double distance = 1 / (checkpointDists.get(u));
+
+						double score = (laps * game.getMaxCheckpoints()) + checkpoints + distance;
+
+						try {
+							if (game.getWinner().equals(u)) {
+								score = score + 1;
+							}
+						} catch (Exception e) {
+						}
+						scores.put(u.getPlayerName(), score);
+					} catch (Exception e) {
+						//User has left
+					}
+				}
+			}
+			player.getInventory().clear();
+
+			player.getInventory().setContents(user.getOldInventory());
+
+			if (!finished) {
+				DoubleValueComparator com = new DoubleValueComparator(scores);
+				SortedMap<String, Double> sorted = new TreeMap<String, Double>(com);
+				sorted.putAll(scores);
+				Set<String> keys = sorted.keySet();
+				Object[] pls = (Object[]) keys.toArray();
+				for (int i = 0; i < pls.length; i++) {
+					Player p = plugin.getServer().getPlayer((String) pls[i]); //Evidence the dodgy PR was not tested as it was still reading string with Player in the map
+					if (p.equals(player)) {
+						if (p != null) {
+							String msg = "";
+							if (!timed) {
+								msg = main.msgs.get("race.end.position");
+								if ((i + 1) <= 4
+										&& (i + 1) != game.getUsers().size()) {player.getWorld().playSound(player.getLocation(), Sound.NOTE_BASS_GUITAR, 1, 1);
+								} else {
+									player.getWorld().playSound(
+											player.getLocation(), Sound.NOTE_BASS,
+											1, 1);
+								}
+								String pos = "" + (i + 1);
+								if (pos.endsWith("1")) {
+									pos = pos + "st";
+								} else if (pos.endsWith("2")) {
+									pos = pos + "nd";
+								} else if (pos.endsWith("3")) {
+									pos = pos + "rd";
+								} else {
+									pos = pos + "th";
+								}
+								msg = msg.replaceAll("%position%", "" + pos);
+							} else {
+								double tim = (game.endTimeMS - game.startTimeMS) / 10;
+								double ti = (int) tim;
+								double t = ti / 100;
+								msg = main.msgs.get("race.end.time");
+								msg = msg.replaceAll(Pattern.quote("%time%"), t
+										+ "");
+								plugin.raceTimes.addRaceTime(game.getTrack()
+										.getTrackName(), player.getName(), t);
+							}
+							p.sendMessage(main.colors.getSuccess() + msg);
+						}
+					}
+				}
+			} else {
+				if (player != null) {
+					int position = 1;
+
+					for (int i = 0; i < game.getUsersFinished().size(); i++) {
+						if (game.getUsersFinished().get(i).equals(user)) {
+							position = i + 1;
+						}
+					}
+					String msg = "";
+					if (!timed) {
+						msg = main.msgs.get("race.end.position");
+						if (position <= 4 && position != game.getUsers().size()) {
+							player.getWorld().playSound(player.getLocation(),
+									Sound.NOTE_BASS_GUITAR, 1, 1);
+						} else {
+							player.getWorld().playSound(player.getLocation(),
+									Sound.NOTE_BASS, 1, 1);
+						}
+						String pos = "" + position;
+						if (pos.endsWith("1")) {
+							pos = pos + "st";
+						} else if (pos.endsWith("2")) {
+							pos = pos + "nd";
+						} else if (pos.endsWith("3")) {
+							pos = pos + "rd";
+						} else {
+							pos = pos + "th";
+						}
+						try {
+							msg = msg.replaceAll("%position%", "" + pos);
+						} catch (Exception e) {
+						}
+					} else {
+						// Time trial
+						double tim = (game.endTimeMS - game.startTimeMS) / 10;
+						double ti = (int) tim;
+						double t = ti / 100;
+						msg = main.msgs.get("race.end.time");
+						msg = msg.replaceAll(Pattern.quote("%time%"), t + "");
+						plugin.raceTimes
+						.addRaceTime(game.getTrack().getTrackName(),
+								player.getName(), t);
+					}
+					player.sendMessage(main.colors.getSuccess() + msg);
+				}
+			}
+			game.leave(user, false);
+			plugin.gameScheduler.updateGame(game);
+			if (game.getUsersIn().size() < 1) {
+				game.ended = true;
+				game.end();
+			}
+			final Player pl = player;
+			plugin.getServer().getScheduler().runTaskLater(plugin, new Runnable() {
+
+				public void run() {
+					String rl = main.config
+							.getString("mariokart.resourceNonMarioPack");
+					Boolean valid = true;
+					try {
+						new URL(rl);
+					} catch (MalformedURLException e2) {
+						valid = false;
+					}
+					if (valid) {
+						pl.sendMessage(main.colors.getInfo()
+								+ main.msgs.get("resource.clear"));
+						pl.setTexturePack(rl);
+					}
+					return;
+				}
+			}, 150l);
+			return;
+		} catch (IllegalArgumentException e) {
+			//Player has left (Silly User system breaking everything...)
+		}
 	}
 
 	@EventHandler
