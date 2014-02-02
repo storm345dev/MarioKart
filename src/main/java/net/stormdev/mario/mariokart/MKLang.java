a1bc53df06368e205450906f03c87a26e749f269
storm345 <storm345dev@gmail.com>
2014-02-02 14:02:09 +0000

    Change up the Lang class

---
+++ src/main/java/net/stormdev/mario/mariokart/MKLang.java
@@ -0,0 +1,258 @@
+package net.stormdev.mario.mariokart;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import net.stormdev.mario.utils.Colors;
+
+public class MKLang {
+	MarioKart plugin = null;
+	private YamlConfiguration lang = new YamlConfiguration();
+
+	public MKLang(MarioKart main) {
+		this.plugin = main;
+		load();
+	}
+
+	public String get(String key) {
+		String val = getRaw(key);
+		val = Colors.colorise(val);
+		return val;
+	}
+
+	public String getRaw(String key) {
+		if (!lang.contains(key)) {
+			return key;
+		}
+		return lang.getString(key);
+	}
+	
+	private void load(){
+		File langFile = new File(plugin.getDataFolder().getAbsolutePath()
+				+ File.separator + "lang.yml");
+		if (langFile.exists() == false || langFile.length() < 1) {
+			try {
+				langFile.createNewFile();
+				// newC.save(configFile);
+			} catch (IOException e) {
+			}
+
+		}
+		try {
+			lang.load(langFile);
+		} catch (Exception e1) {
+			plugin.getLogger().log(Level.WARNING,
+					"Error creating/loading lang file! Regenerating..");
+		}
+		// Setup the Lang file
+		if (!lang.contains("error.memoryLockdown")) {
+			lang.set("error.memoryLockdown",
+					"Operation failed due to lack of System Memory!");
+		}
+		if (!lang.contains("general.disabled")) {
+			lang.set("general.disabled",
+					"Error: Disabled");
+		}
+		if (!lang.contains("general.cmd.leave.success")) {
+			lang.set("general.cmd.leave.success",
+					"Successfully left %name%!");
+		}
+		if (!lang.contains("general.cmd.page")) {
+			lang.set("general.cmd.page", "Page [%page%/%total%]:");
+		}
+		if (!lang.contains("general.cmd.full")) {
+			lang.set("general.cmd.full",
+					"There are no race tracks available!");
+		}
+		if (!lang.contains("general.cmd.overflow")) {
+			lang.set("general.cmd.overflow",
+					"Queues/Tracks are full, joining new low-priority queue!");
+		}
+		if (!lang.contains("general.cmd.playersOnly")) {
+			lang.set("general.cmd.playersOnly",
+					"This command is for players only!");
+		}
+		if (!lang.contains("general.cmd.leave.fail")) {
+			lang.set("general.cmd.leave.fail", "You aren't in a game/que!");
+		}
+		if (!lang.contains("general.cmd.setlaps.success")) {
+			lang.set("general.cmd.setlaps.success",
+					"Successfully set laps for track %name%!");
+		}
+		if (!lang.contains("general.cmd.delete.success")) {
+			lang.set("general.cmd.delete.success",
+					"Successfully deleted track %name%!");
+		}
+		if (!lang.contains("general.cmd.delete.exists")) {
+			lang.set("general.cmd.delete.exists",
+					"That track doesn't exist!");
+		}
+		if (!lang.contains("general.cmd.racetimes")) {
+			lang.set("general.cmd.racetimes",
+					"Top %n% times for track %track%:");
+		}
+		if (!lang.contains("general.shop.notEnoughMoney")) {
+			lang.set("general.shop.notEnoughMoney",
+					"You don't have enough %currency% for that item!");
+		}
+		if (!lang.contains("general.shop.maxUpgrades")) {
+			lang.set("general.shop.maxUpgrades",
+					"You are not allowed to own more than 64 of an upgrade!");
+		}
+		if (!lang.contains("general.shop.success")) {
+			lang.set(
+					"general.shop.success",
+					"Successfully bought %name% for %price% %currency%! You now have %balance% %currency%!");
+		}
+		if (!lang.contains("general.shop.sellSuccess")) {
+			lang.set("general.shop.sellSuccess",
+					"Successfully removed %amount% of %name% from your upgrades list!");
+		}
+		if (!lang.contains("general.shop.error")) {
+			lang.set("general.shop.error",
+					"An error occured. Please contact a member of staff. (No economy found)");
+		}
+		if (!lang.contains("setup.create.exists")) {
+			lang.set("setup.create.exists",
+					"This track already exists! Please do /urace delete %name% before proceeding!");
+		}
+		if (!lang.contains("setup.create.start")) {
+			lang.set("setup.create.start", "Wand: %id% (%name%)");
+		}
+		if (!lang.contains("setup.create.lobby")) {
+			lang.set("setup.create.lobby",
+					"Stand in the lobby and right click anywhere with the wand");
+		}
+		if (!lang.contains("setup.create.exit")) {
+			lang.set("setup.create.exit",
+					"Stand at the track exit and right click anywhere with the wand");
+		}
+		if (!lang.contains("setup.create.grid")) {
+			lang.set(
+					"setup.create.grid",
+					"Stand where you want a car to start the race and right click anywhere (Without the wand). Repeat for all the starting positions. When done, right click anywhere with the wand");
+		}
+		if (!lang.contains("setup.create.checkpoints")) {
+			lang.set(
+					"setup.create.checkpoints",
+					"Stand at each checkpoint along the track (Checkpoint 10x10 radius) and right click anywhere (Without the wand). Repeat for all checkpoints. When done, right click anywhere with the wand");
+		}
+		if (!lang.contains("setup.create.notEnoughCheckpoints")) {
+			lang.set("setup.create.notEnoughCheckpoints",
+					"You must have at least 3 checkpoints! You only have: %num%");
+		}
+		if (!lang.contains("setup.create.line1")) {
+			lang.set(
+					"setup.create.line1",
+					"Stand at one end of the start/finish line and right click anywhere with the wand");
+		}
+		if (!lang.contains("setup.create.line2")) {
+			lang.set(
+					"setup.create.line2",
+					"Stand at the other end of the start/finish line and right click anywhere with the wand");
+		}
+		if (!lang.contains("setup.create.done")) {
+			lang.set("setup.create.done",
+					"Successfully created Race Track %name%!");
+		}
+		if (!lang.contains("setup.fail.queueSign")) {
+			lang.set("setup.fail.queueSign",
+					"That track doesn't exist!");
+		}
+		if (!lang.contains("setup.create.queueSign")) {
+			lang.set("setup.create.queueSign",
+					"Successfully registered queue sign!");
+		}
+		if (!lang.contains("race.que.existing")) {
+			lang.set("race.que.existing",
+					"You are already in a game/que! Please leave it before joining this one!");
+		}
+		if (!lang.contains("race.que.other")) {
+			lang.set("race.que.other",
+					"Unavailable! Current queue race type: %type%");
+		}
+		if (!lang.contains("race.que.full")) {
+			lang.set("race.que.full", "Race que full!");
+		}
+		if (!lang.contains("race.que.success")) {
+			lang.set("race.que.success", "In Race Que!");
+		}
+		if (!lang.contains("race.que.joined")) {
+			lang.set("race.que.joined", " joined the race que!");
+		}
+		if (!lang.contains("race.que.left")) {
+			lang.set("race.que.left", " left the race que!");
+		}
+		if (!lang.contains("race.que.players")) {
+			lang.set(
+					"race.que.players",
+					"Acquired minimum players for race! Waiting %time% seconds for additional players to join...");
+		}
+		if (!lang.contains("race.que.preparing")) {
+			lang.set("race.que.preparing", "Preparing race...");
+		}
+		if (!lang.contains("race.que.starting")) {
+			lang.set("race.que.starting", "Race starting in...");
+		}
+		if (!lang.contains("resource.download")) {
+			lang.set("resource.download", "Downloading resources...");
+		}
+		if (!lang.contains("resource.downloadHelp")) {
+			lang.set("resource.downloadHelp",
+					"If the resources aren't downloaded automatically. Download it at: %url%");
+		}
+		if (!lang.contains("resource.clear")) {
+			lang.set("resource.clear",
+					"Switching back to default minecraft textures...");
+		}
+		if (!lang.contains("race.que.go")) {
+			lang.set("race.que.go", "Go!");
+		}
+		if (!lang.contains("race.end.timeLimit")) {
+			lang.set("race.end.timeLimit", "Time Limit exceeded!");
+		}
+		if (!lang.contains("race.end.won")) {
+			lang.set("race.end.won", " won the race!");
+		}
+		if (!lang.contains("race.end.rewards")) {
+			lang.set("race.end.rewards",
+					"&6+&a%amount%&6 %currency% for %position%! You now have %balance% %currency%!");
+		}
+		if (!lang.contains("race.end.time")) {
+			lang.set("race.end.time", "Your time was %time% seconds!");
+		}
+		if (!lang.contains("race.mid.miss")) {
+			lang.set("race.mid.miss",
+					"You missed a section of the track! Please go back and do it!");
+		}
+		if (!lang.contains("race.mid.backwards")) {
+			lang.set("race.mid.backwards", "You are going the wrong way!");
+		}
+		if (!lang.contains("race.mid.lap")) {
+			lang.set("race.mid.lap", "Lap [%lap%/%total%]");
+		}
+		if (!lang.contains("race.end.soon")) {
+			lang.set("race.end.soon",
+					"You have 1 minute before the race ends!");
+		}
+		if (!lang.contains("race.end.position")) {
+			lang.set("race.end.position", "You finished %position%!");
+		}
+		if (!lang.contains("race.upgrades.use")) {
+			lang.set("race.upgrades.use", "&c[-]&6 Consumed Upgrade");
+		}
+		if (!lang.contains("mario.hit")) {
+			lang.set("mario.hit", "You were hit by a %name%!");
+		}
+		try {
+			lang.save(langFile);
+		} catch (IOException e1) {
+			plugin.getLogger().info("Error saving lang file!");
+		} catch (Exception e2){
+			plugin.getLogger().info("Error parsing lang file!");
+		}
+	}
+}
--- src/main/java/net/stormdev/mario/mariokart/MarioKart.java
+++ src/main/java/net/stormdev/mario/mariokart/MarioKart.java
@@ -13,7 +13,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
@@ -30,7 +29,6 @@ import net.stormdev.mario.events.ServerEventsListener;
 import net.stormdev.mario.events.SignEventsListener;
 import net.stormdev.mario.events.TrackEventsListener;
 import net.stormdev.mario.hotbar.HotBarManager;
-import net.stormdev.mario.hotbar.HotBarUpgrade;
 import net.stormdev.mario.lesslag.DynamicLagReducer;
 import net.stormdev.mario.powerups.PowerupManager;
 import net.stormdev.mario.queues.RaceQueue;
@@ -47,8 +45,6 @@ import net.stormdev.mario.tracks.RaceTimes;
 import net.stormdev.mario.tracks.RaceTrackManager;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
@@ -65,7 +61,6 @@ import com.useful.ucars.Colors;
 import com.useful.ucars.ucars;
 
 public class MarioKart extends JavaPlugin {
-	public YamlConfiguration lang = new YamlConfiguration();
 	public static MarioKart plugin;
 	public static FileConfiguration config = new YamlConfiguration();
 	public static Colors colors;
@@ -79,7 +74,7 @@ public class MarioKart extends JavaPlugin {
 	public RaceScheduler raceScheduler = null;
 	public ConcurrentHashMap<String, LinkedHashMap<UUID, RaceQueue>> queues = new ConcurrentHashMap<String, LinkedHashMap<UUID, RaceQueue>>();
 	public RaceQueueManager raceQueues = null;
-	public static Lang msgs = null;
+	public static MKLang msgs = null;
 	public RaceMethods raceMethods = null;
 	public Random random = null;
 	public static PowerupManager powerupManager = null;
@@ -144,23 +139,7 @@ public class MarioKart extends JavaPlugin {
 		plugin = this;
 		File queueSignFile = new File(getDataFolder().getAbsolutePath()
 				+ File.separator + "Data" + File.separator + "queueSigns.signData");
-		File langFile = new File(getDataFolder().getAbsolutePath()
-				+ File.separator + "lang.yml");
-		if (langFile.exists() == false || langFile.length() < 1) {
-			try {
-				langFile.createNewFile();
-				// newC.save(configFile);
-			} catch (IOException e) {
-			}
-
-		}
-		try {
-			lang.load(langFile);
-		} catch (Exception e1) {
-			getLogger().log(Level.WARNING,
-					"Error creating/loading lang file! Regenerating..");
-		}
-		msgs = new Lang(this);
+		msgs = new MKLang(this);
 		if (new File(getDataFolder().getAbsolutePath() + File.separator
 				+ "config.yml").exists() == false
 				|| new File(getDataFolder().getAbsolutePath() + File.separator
@@ -177,216 +156,11 @@ public class MarioKart extends JavaPlugin {
 		config = getConfig();
 		logger = new CustomLogger(getServer().getConsoleSender(), getLogger());
 		try {
-			// Setup the Lang file
-			if (!lang.contains("error.memoryLockdown")) {
-				lang.set("error.memoryLockdown",
-						"Operation failed due to lack of System Memory!");
-			}
-			if (!lang.contains("general.disabled")) {
-				lang.set("general.disabled",
-						"Error: Disabled");
-			}
-			if (!lang.contains("general.cmd.leave.success")) {
-				lang.set("general.cmd.leave.success",
-						"Successfully left %name%!");
-			}
-			if (!lang.contains("general.cmd.page")) {
-				lang.set("general.cmd.page", "Page [%page%/%total%]:");
-			}
-			if (!lang.contains("general.cmd.full")) {
-				lang.set("general.cmd.full",
-						"There are no race tracks available!");
-			}
-			if (!lang.contains("general.cmd.overflow")) {
-				lang.set("general.cmd.overflow",
-						"Queues/Tracks are full, joining new low-priority queue!");
-			}
-			if (!lang.contains("general.cmd.playersOnly")) {
-				lang.set("general.cmd.playersOnly",
-						"This command is for players only!");
-			}
-			if (!lang.contains("general.cmd.leave.fail")) {
-				lang.set("general.cmd.leave.fail", "You aren't in a game/que!");
-			}
-			if (!lang.contains("general.cmd.setlaps.success")) {
-				lang.set("general.cmd.setlaps.success",
-						"Successfully set laps for track %name%!");
-			}
-			if (!lang.contains("general.cmd.delete.success")) {
-				lang.set("general.cmd.delete.success",
-						"Successfully deleted track %name%!");
-			}
-			if (!lang.contains("general.cmd.delete.exists")) {
-				lang.set("general.cmd.delete.exists",
-						"That track doesn't exist!");
-			}
-			if (!lang.contains("general.cmd.racetimes")) {
-				lang.set("general.cmd.racetimes",
-						"Top %n% times for track %track%:");
-			}
-			if (!lang.contains("general.shop.notEnoughMoney")) {
-				lang.set("general.shop.notEnoughMoney",
-						"You don't have enough %currency% for that item!");
-			}
-			if (!lang.contains("general.shop.maxUpgrades")) {
-				lang.set("general.shop.maxUpgrades",
-						"You are not allowed to own more than 64 of an upgrade!");
-			}
-			if (!lang.contains("general.shop.success")) {
-				lang.set(
-						"general.shop.success",
-						"Successfully bought %name% for %price% %currency%! You now have %balance% %currency%!");
-			}
-			if (!lang.contains("general.shop.sellSuccess")) {
-				lang.set("general.shop.sellSuccess",
-						"Successfully removed %amount% of %name% from your upgrades list!");
-			}
-			if (!lang.contains("general.shop.error")) {
-				lang.set("general.shop.error",
-						"An error occured. Please contact a member of staff. (No economy found)");
-			}
-			if (!lang.contains("setup.create.exists")) {
-				lang.set("setup.create.exists",
-						"This track already exists! Please do /urace delete %name% before proceeding!");
-			}
-			if (!lang.contains("setup.create.start")) {
-				lang.set("setup.create.start", "Wand: %id% (%name%)");
-			}
-			if (!lang.contains("setup.create.lobby")) {
-				lang.set("setup.create.lobby",
-						"Stand in the lobby and right click anywhere with the wand");
-			}
-			if (!lang.contains("setup.create.exit")) {
-				lang.set("setup.create.exit",
-						"Stand at the track exit and right click anywhere with the wand");
-			}
-			if (!lang.contains("setup.create.grid")) {
-				lang.set(
-						"setup.create.grid",
-						"Stand where you want a car to start the race and right click anywhere (Without the wand). Repeat for all the starting positions. When done, right click anywhere with the wand");
-			}
-			if (!lang.contains("setup.create.checkpoints")) {
-				lang.set(
-						"setup.create.checkpoints",
-						"Stand at each checkpoint along the track (Checkpoint 10x10 radius) and right click anywhere (Without the wand). Repeat for all checkpoints. When done, right click anywhere with the wand");
-			}
-			if (!lang.contains("setup.create.notEnoughCheckpoints")) {
-				lang.set("setup.create.notEnoughCheckpoints",
-						"You must have at least 3 checkpoints! You only have: %num%");
-			}
-			if (!lang.contains("setup.create.line1")) {
-				lang.set(
-						"setup.create.line1",
-						"Stand at one end of the start/finish line and right click anywhere with the wand");
-			}
-			if (!lang.contains("setup.create.line2")) {
-				lang.set(
-						"setup.create.line2",
-						"Stand at the other end of the start/finish line and right click anywhere with the wand");
-			}
-			if (!lang.contains("setup.create.done")) {
-				lang.set("setup.create.done",
-						"Successfully created Race Track %name%!");
-			}
-			if (!lang.contains("setup.fail.queueSign")) {
-				lang.set("setup.fail.queueSign",
-						"That track doesn't exist!");
-			}
-			if (!lang.contains("setup.create.queueSign")) {
-				lang.set("setup.create.queueSign",
-						"Successfully registered queue sign!");
-			}
-			if (!lang.contains("race.que.existing")) {
-				lang.set("race.que.existing",
-						"You are already in a game/que! Please leave it before joining this one!");
-			}
-			if (!lang.contains("race.que.other")) {
-				lang.set("race.que.other",
-						"Unavailable! Current queue race type: %type%");
-			}
-			if (!lang.contains("race.que.full")) {
-				lang.set("race.que.full", "Race que full!");
-			}
-			if (!lang.contains("race.que.success")) {
-				lang.set("race.que.success", "In Race Que!");
-			}
-			if (!lang.contains("race.que.joined")) {
-				lang.set("race.que.joined", " joined the race que!");
-			}
-			if (!lang.contains("race.que.left")) {
-				lang.set("race.que.left", " left the race que!");
-			}
-			if (!lang.contains("race.que.players")) {
-				lang.set(
-						"race.que.players",
-						"Acquired minimum players for race! Waiting %time% seconds for additional players to join...");
-			}
-			if (!lang.contains("race.que.preparing")) {
-				lang.set("race.que.preparing", "Preparing race...");
-			}
-			if (!lang.contains("race.que.starting")) {
-				lang.set("race.que.starting", "Race starting in...");
-			}
-			if (!lang.contains("resource.download")) {
-				lang.set("resource.download", "Downloading resources...");
-			}
-			if (!lang.contains("resource.downloadHelp")) {
-				lang.set("resource.downloadHelp",
-						"If the resources aren't downloaded automatically. Download it at: %url%");
-			}
-			if (!lang.contains("resource.clear")) {
-				lang.set("resource.clear",
-						"Switching back to default minecraft textures...");
-			}
-			if (!lang.contains("race.que.go")) {
-				lang.set("race.que.go", "Go!");
-			}
-			if (!lang.contains("race.end.timeLimit")) {
-				lang.set("race.end.timeLimit", "Time Limit exceeded!");
-			}
-			if (!lang.contains("race.end.won")) {
-				lang.set("race.end.won", " won the race!");
-			}
-			if (!lang.contains("race.end.rewards")) {
-				lang.set("race.end.rewards",
-						"&6+&a%amount%&6 %currency% for %position%! You now have %balance% %currency%!");
-			}
-			if (!lang.contains("race.end.time")) {
-				lang.set("race.end.time", "Your time was %time% seconds!");
-			}
-			if (!lang.contains("race.mid.miss")) {
-				lang.set("race.mid.miss",
-						"You missed a section of the track! Please go back and do it!");
-			}
-			if (!lang.contains("race.mid.backwards")) {
-				lang.set("race.mid.backwards", "You are going the wrong way!");
-			}
-			if (!lang.contains("race.mid.lap")) {
-				lang.set("race.mid.lap", "Lap [%lap%/%total%]");
-			}
-			if (!lang.contains("race.end.soon")) {
-				lang.set("race.end.soon",
-						"You have 1 minute before the race ends!");
-			}
-			if (!lang.contains("race.end.position")) {
-				lang.set("race.end.position", "You finished %position%!");
-			}
-			if (!lang.contains("race.upgrades.use")) {
-				lang.set("race.upgrades.use", "&c[-]&6 Consumed Upgrade");
-			}
-			if (!lang.contains("mario.hit")) {
-				lang.set("mario.hit", "You were hit by a %name%!");
-			}
 			// Setup the config
 			PluginConfigurator.load(config); //Loads and converts configs
 		} catch (Exception e) {
 		}
 		saveConfig();
-		try {
-			lang.save(langFile);
-		} catch (IOException e1) {
-			getLogger().info("Error parsing lang file!");
-		}
 		uCarsAPI.getAPI().hookPlugin(this);
 		// Load the colour scheme
 		colors = new Colors(config.getString("colorScheme.success"),
