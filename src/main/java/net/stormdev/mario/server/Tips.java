73de79bed2b4b9a616ce3dc1531f34e848984994
storm345 <storm345dev@gmail.com>
2014-06-15 17:35:46 +0100

    Added some more tips

--- src/main/java/net/stormdev/mario/server/Tips.java
+++ src/main/java/net/stormdev/mario/server/Tips.java
@@ -6,14 +6,18 @@ public class Tips {
 	private static final String[] tips = new String[]{
 		"Use brake (d) to get around sharp corners!",
 		"Use brake (d) to block against POW blocks!",
-		"Hold forwards after 3s to go for a boost!",
+		"Hold forwards 3s before the start for a boost!",
 		"Right click to use a powerup!",
 		"Drive over a box to get a powerup!",
 		"Use the door to quit back to the lobby!",
 		"Use the egg to respawn if you get stuck or lost!",
 		"Buy upgrades in the race shop with tokens!",
 		"Win the race to get tokens!",
-		"Drive over gold and diamond blocks to boost!"
+		"Drive over gold and diamond blocks to boost!",
+		"Use left click to fire green shells forwards!",
+		"Use right click to fire green shells backwards!",
+		"Make sure sound is on when you play!",
+		"Make sure particles are on when you play!"
 	};
 	
 	private static String tip = random();
